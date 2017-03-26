58ea1487ee69449352846f29022082e3cb9ca710
Phillip Sutherland <Sutherlandp@NA.Group.Local>
2017-03-26 22:30:53 +0200

    update

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+class PageDisplay(admin.ModelAdmin):
+    list_display = ('title','category','url')
+
+admin.site.register(Category)
+admin.site.register(Page, PageDisplay)
--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from rango.models import Category
 
 def index(request):
-    return HttpResponse("rango app")
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html',context=None)
